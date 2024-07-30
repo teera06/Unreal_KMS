e5e76462f32d63eefdc4f35804807bbcaa8cdafa
민승 김 <rlaalstmd0606@gmail.com>
2024-07-30 11:20:35 +0900

    김민승 : 몬스터 정리

---
+++ Source/Unreal_Project/MainActor/MainMonster.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GlobalMainCharacter/GlobalCharacter.h"
+#include "MainMonster.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL_PROJECT_API AMainMonster : public AGlobalCharacter
+{
+	GENERATED_BODY()
+	
+};
