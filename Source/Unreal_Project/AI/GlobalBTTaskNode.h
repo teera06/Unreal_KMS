cbf6afc6129ba98b4ff24325b21c1fe4a2994219
민승 김 <rlaalstmd0606@gmail.com>
2024-07-30 11:49:22 +0900

    김민승 :  중간 커밋

---
+++ Source/Unreal_Project/AI/GlobalBTTaskNode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "GlobalBTTaskNode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL_PROJECT_API UGlobalBTTaskNode : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+};
