a3b42169134cd0662f14e9e572bbae97e79fce9e
민승 김 <rlaalstmd0606@gmail.com>
2024-08-02 15:16:16 +0900

    김민승 : 다시 커밋

---
+++ Source/Unreal_Project/Global/GlobalGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GlobalGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL_PROJECT_API AGlobalGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
